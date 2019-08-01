99c20887dea39883e1550583b715713af5215a10
anishch <anish.chaudhuri@hotmail.com>
2019-08-01 15:47:36 -0700

    Did a lot of major edits - better finish tonight and if that doesn't happen...

--- StringSearch/StringSearch.c
+++ StringSearch/StringSearch.c
@@ -3,10 +3,8 @@
 #include <stdlib.h>
 
 #define MAX_LENGTH 100000
-
-int splitEntire() {
-	
-}
+#define MAX_WORDS 33;
+#define MAX_WORD_LENGTH 45;
 
 int main()
 {
@@ -27,15 +25,159 @@ int main()
 				wordlength[z] = searchTermincrement;
 		   }
 		   else if (z > 0){
-			   wordlength[z] = searchTermincrement - wordlength[z - 1];
-		   z++; //math here is wrong
+			   wordlength[z] = searchTermincrement - wordlength[z - 1] - 1;
 		   }
+		   z++; //math here may be wrong
 	   }
    }
    numwordsinEntire = numwordsinEntire + 1; 
+   printf("%d\n", numwordsinEntire);
+   printf("%d\n", wordlength[1]);
+   int i = 0;
+   char arrayofwords[33][45] = { 0 };
    for (int wordbyword = 0; wordbyword < numwordsinEntire; wordbyword++) {
-	   char* words = malloc(wordlength[wordbyword] * sizeof(char));
-	   words++;
+	   char* currentWord = malloc(wordlength[wordbyword] * sizeof(char));
+	   while (*currentWord < wordlength[wordbyword]) {
+		   if (*currentWord != ' ') {
+			   *currentWord = entire[wordbyword];
+			   currentWord++;
+		   }
+		   else if (entire[wordbyword] == ' ') {
+			   *currentWord = NULL;
+			   wordbyword++;
+		   }
+	   }
+	   strcpy_s(arrayofwords[i], 45, currentWord);
+	   i++; 
+   }
+   int m = 0;
+   int numblocksinInput = 0;
+   int articlelength[MAX_LENGTH];
+   char arrayofarticles[45][MAX_LENGTH];
+   for (int blocksoftext = 0; blocksoftext <= strlen(input); blocksoftext++) {
+	   if (input[blocksoftext] == '@') {
+		   numblocksinInput++;
+		   if (m == 0) {
+			   articlelength[m] = blocksoftext - 1;
+		   }
+		   else if (m > 0) {
+			   articlelength[m] = blocksoftext - wordlength[m - 1] - 1;
+		   }
+		   m++; //math here may be wrong
+	   }
+   }
+   numblocksinInput = numblocksinInput + 1;
+   int l = 0;
+   for (int articlebyarticle = 0; articlebyarticle < numblocksinInput; articlebyarticle++) {
+	   char* currentarticle = malloc(articlelength[articlebyarticle] * sizeof(char));
+	   while (*currentarticle < articlelength[articlebyarticle]) {
+		   if (*currentarticle != '@') {
+			   *currentarticle = input[articlebyarticle];
+			   articlebyarticle++;
+		   }
+		   else if (input[articlebyarticle] == '@') {
+			   *currentarticle == NULL;
+			   articlebyarticle++;
+		   }
+	   }
+	   strcpy_s(arrayofarticles[l], MAX_LENGTH, currentarticle);
+	   l++;
+   }
+   int s = 0;
+   int q = 0;
+   int count = 0;
+   for (int b = 0; b < l; b++) { //creating for loop for all articles
+	   for (int i = 0; i <= (strlen(input) + 1); i++) { //creating for loop for all characters per article
+		   if (entire[s] != arrayofarticles[b][i]) { //if they are not equal
+			   s = 0; //whatever we've counted, it's not applicable if the whole term doesn't match
+			   if (input[i] == NULL && s == 0) { //if nothing is there for Article #B, move on to Article #B + 1
+				   continue; 
+			   }
+		   }
+		   else if (entire[s] == arrayofarticles[b][i]) {
+			   s++;
+			   if (entire[s] == NULL) {
+				   q++;
+				   for (int z = 0; z <= (strlen(input) + 1); z++){
+					   printf("%c", arrayofarticles[b][z]);
+				   }
+				   printf("\n");
+			   }
+		   }
+	   }
+   }
+}
+
+
+if (q < 3) {
+	printf("Suggested Results\n");
+	printf("-----------------\n");
+	for (int x = 0; x < l; x) { //loops over each article
+		for (int w = 0; w < 33; w) { //loops over each word in search term
+			for (int p = 0; p < 45; p) { //loops over each character in each word in search term
+				for (int u = 0; u < MAX_LENGTH; u++) { //loops over each character in article
+					if (arrayofwords[w][p] != arrayofarticles[x][u]{ //if they aren't equal to each other
+						p = 0; //set p to 0
+						if (arrayofarticles[x][u] = NULL) {
+							w++; //look at next word
+							u = 0; //start over for original article
+							break;  
+						}
+					}
+					else if (arrayofwords[w][p] == arrayofarticles[x][u]) {
+						p++;
+					}
+				}
+			}
+		}
+	}
+}
+
+//strcmp
+
+/*
+int main()
+{
+	char input[MAX_LENGTH];
+	char entire[MAX_LENGTH];
+	printf("StringSearch Initialized\n");
+   printf("Please input the text in which you would like to search. (Max 1000 characters)\n");
+   gets_s(input, MAX_LENGTH);
+   printf("Please enter your search terms\n");
+   gets_s(entire, MAX_LENGTH);
+  int numwordsinEntire = 0;
+  int z = 0;
+  int wordlength[33];
+   for (int searchTermincrement = 0; searchTermincrement <= strlen(entire); searchTermincrement++) {
+	   if (entire[searchTermincrement] == ' ') {
+		   numwordsinEntire++;
+		   if (z == 0) {
+				wordlength[z] = searchTermincrement;
+		   }
+		   else if (z > 0){
+			   wordlength[z] = searchTermincrement - wordlength[z - 1] - 1;
+		   }
+		   z++; //math here is wrong
+	   }
+   }
+   numwordsinEntire = numwordsinEntire + 1; 
+   printf("%d\n", numwordsinEntire);
+   printf("%d\n", wordlength[1]);
+   int i = 0;
+   char arrayofwords[33][45] = { 0 };
+   for (int wordbyword = 0; wordbyword < numwordsinEntire; wordbyword++) {
+	   char* currentWord = malloc(wordlength[wordbyword] * sizeof(char));
+	   for (*currentWord < wordlength[wordbyword]) {
+		   if (*currentWord != ' ') {
+			   *currentWord = entire[wordbyword];
+			   currentWord++;
+		   }
+		   else if (entire[wordbyword] == ' ') {
+			   wordbyword++;
+		   }
+	   }
+	   strcpy(arrayofwords[i], currentWord);
+	   i++; 
    }
    int s = 0;
    int q = 0;
@@ -58,7 +200,10 @@ int main()
 	   printf("Congratulations, your string is included in your overall block of text!\n");
    }
    printf("Count: %d", q);
-}
+   if (q = 0) {
+
+   }
+}*/
 
 
 /*
